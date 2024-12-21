ff9de5230daaf9e7260327e90e885f58c7c1bfc3
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-12-21 21:22:24 +0900

    Add eslint-plugin-import to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  importPlugin.flatConfigs.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
